4652c1b450c81daeba48fa2d1eaea880976d3db5
udms100 <infoxfinesnig@gmail.com>
2019-06-01 22:57:08 +0100

    First commit

---
+++ src/models/Car.js
@@ -0,0 +1,17 @@
+// External Dependacies
+const mongoose = require('mongoose')
+
+const carSchema = new mongoose.Schema({
+   
+    title: String,
+    brand: String,
+    price: String,
+    age: Number,
+    service: {
+        type: Map,
+        of: String
+    }
+
+})
+
+module.exports = mongoose.model('Car', carSchema)
